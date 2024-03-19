5958571aab5a5ec777a61cfcb9fd6b5d0a665efb
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-19 14:48:06 -0300

    feat(model): autor

--- livraria/models.py
+++ livraria/models.py
@@ -14,3 +14,16 @@ class Editora(models.Model):
 
     def __str__(self) -> str:
         return self.nome
+
+class Autor(models.Model):
+    nome: str = models.CharField(max_length=255)
+    email: str = models.EmailField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.nome
+
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
+
+
