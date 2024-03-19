2f71d147901375781ec214b44ae6a6cbbc006f92
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-19 14:59:37 -0300

    feat(livro): categoria field

--- livraria/models.py
+++ livraria/models.py
@@ -31,6 +31,9 @@ class Livro(models.Model):
     isbn: str = models.CharField(max_length=32, null=True, blank=True)
     quantidade: int = models.IntegerField(default=0, null=True, blank=True)
     preco: float = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    categoria: Categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
 
     def __str__(self) -> str:
         return f"{self.titulo} ({self.quantidade})"
