501f0a9a6928bf85d427083a7b5895868f3fd448
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-19 15:32:20 -0300

    feat(api): serializers, viewsets and urls

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,13 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from livraria.views import CategoriaViewSet, EditoraViewSet
+from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
+router.register(r"autor", AutorViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
+router.register(r"livro", LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- livraria/serializers.py
+++ livraria/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from livraria.models import Categoria, Editora
+from livraria.models import Autor, Categoria, Editora, Livro
 
 
 class CategoriaSerializer(ModelSerializer):
@@ -12,3 +12,28 @@ class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
         fields = "__all__"
+
+
+class AutorSerializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = "__all__"
+
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
+
+
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ['id','titulo','preco']
