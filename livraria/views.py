17e4483abf5bc4fe8c9f9a136baa96bd68d764bc
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-19 15:17:52 -0300

    feat(api): editora class

--- livraria/views.py
+++ livraria/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria
-from livraria.serializers import CategoriaSerializer
+from livraria.models import Categoria, Editora
+from livraria.serializers import CategoriaSerializer, EditoraSerializer
 
 
 # Create your views here.
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
