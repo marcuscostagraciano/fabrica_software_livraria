08dc64e14b4d0450e33be066645bac7952e6c6a0
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-19 15:11:27 -0300

    feat(view): categoria view set

--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from livraria.models import Categoria
+from livraria.serializers import CategoriaSerializer
+
 
 # Create your views here.
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
