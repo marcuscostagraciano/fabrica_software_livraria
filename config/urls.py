efb1081e20298e6c4201a3d94acc258cf4e6b8b5
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-20 15:44:04 -0300

    fix(url route): autores

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
-router.register(r"autor", AutorViewSet)
+router.register(r"autores", AutorViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
 router.register(r"livros", LivroViewSet)
