ec5a394cb4440c2b2bc9b8160ce5b32735935806
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-22 15:40:32 -0300

    passo 28: visualizacao itens compra

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,12 +1,27 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import (ModelSerializer,
+                                        CharField,
+                                        SerializerMethodField)
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ["livro", "quantidade", "total"]
+        depth = 2
+
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.quantidade * instance.livro.preco
 
 
 class CompraSerializer(ModelSerializer):
-    usuario = CharField(source="usuario.email", read_only=True)
-    status = CharField(source="get_status_display", read_only=True)
-
     class Meta:
         model = Compra
         fields = "__all__"
+
+    usuario = CharField(source="usuario.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
