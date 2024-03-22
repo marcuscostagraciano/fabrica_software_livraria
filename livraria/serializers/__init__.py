048bcec371895428ffc4906ba44a0626dcf5f561
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-22 20:26:34 -0300

    passo 30: endpoint criacao novas compras

--- livraria/serializers/__init__.py
+++ livraria/serializers/__init__.py
@@ -3,4 +3,4 @@ from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroSerializer, LivroDetailSerializer, LivroListSerializer
 from .usuario import UsuarioSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer, CriarEditarCompraSerializer
--- livraria/views/compra.py
+++ livraria/views/compra.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Compra
-from livraria.serializers import CompraSerializer
+from livraria.serializers import CompraSerializer, CriarEditarCompraSerializer
 
 
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
     serializer_class = CompraSerializer
+
+    def get_serializer_class(self):
+        if self.action in ("create", "update"):
+            return CriarEditarCompraSerializer
+        return CompraSerializer
