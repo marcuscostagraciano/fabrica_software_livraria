d2b5cb861f4d3e03da7c1d3761d63cda763ddd55
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-23 11:52:44 -0300

    passo 37: busca textual nos livros

--- livraria/views/livro.py
+++ livraria/views/livro.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from livraria.models import Livro
@@ -9,8 +10,9 @@ from livraria.serializers import (LivroSerializer, LivroDetailSerializer,
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["categoria__descricao"]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["categoria__descricao", "editora__nome"]
+    search_fields = ["titulo"]
 
     def get_serializer_class(self):
         if self.action == "list":
