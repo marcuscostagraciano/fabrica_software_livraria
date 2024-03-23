11030003a411f86101995e7c2c188edb6272f320
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-23 13:23:29 -0300

    comentários didáticos

--- livraria/views/livro.py
+++ livraria/views/livro.py
@@ -11,9 +11,13 @@ class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # busca pelo(s) campo(s) declarado(s)
     filterset_fields = ["categoria__descricao", "editora__nome"]
+    # busca quando usar o search=
     search_fields = ["titulo"]
+    # possíveis filtros
     ordering_fields = ["titulo", "preco"]
+    # filtro padrão
     ordering = ["titulo"]
 
     def get_serializer_class(self):
