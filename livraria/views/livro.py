987443b0ac50ca102ebc634ac52a38cec9e7af6a
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-23 00:11:31 -0300

    passo 36: filtro de livros

--- livraria/views/livro.py
+++ livraria/views/livro.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 from livraria.models import Livro
 from livraria.serializers import (LivroSerializer, LivroDetailSerializer,
@@ -7,6 +8,9 @@ from livraria.serializers import (LivroSerializer, LivroDetailSerializer,
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["categoria__descricao"]
 
     def get_serializer_class(self):
         if self.action == "list":
